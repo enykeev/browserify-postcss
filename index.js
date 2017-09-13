99f339fe6cfb5b9ff24c56dff7b5faf965518c2b
Kirill Izotov <enykeev@stackstorm.com>
2017-09-13 16:03:44 +0700

    Support insert-css injector and modularization

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ var PassThrough = require('stream').PassThrough
 var resolve = require('resolve')
 var postcss = require('postcss')
 var path = require('path')
+var Core = require('css-modules-loader-core')
+
+function getModuleName(file) {
+  return new Promise(function (resolves, rejects) {
+    resolve(file, {}, function (err, res, pkg) {
+      if (err) {
+        return rejects(err)
+      }
+      return resolves(pkg.name)
+    })
+  })
+}
 
 module.exports = function (file, opts) {
   opts = opts || {}
@@ -19,23 +31,41 @@ module.exports = function (file, opts) {
   postCssOptions.from = postCssOptions.from || file
   postCssOptions.to = postCssOptions.to || file
 
-  var parser = opts.parser;
+  var parser = opts.parser
   if (parser) {
     if (typeof parser === 'string') {
       parser = require(
         resolve.sync(String(parser), {
-          basedir: opts.basedir || process.cwd()
+          basedir: opts.basedir || process.cwd(),
         })
-      );
+      )
     }
-    postCssOptions.parser = parser;
+    postCssOptions.parser = parser
   }
 
   return sink.str(function (body, done) {
     var self = this
+    var exports
     processor.process(body, postCssOptions)
       .then(function (result) {
-        self.push(moduleify(result.css, opts.inject))
+        if (!opts.modularize) {
+          return result.css
+        }
+
+        return getModuleName(file)
+          .then(function (modulename) {
+            return moduleify(result.css, modulename, true)
+          })
+      })
+      .then(function (result) {
+        return insert(result, opts.inject)
+      })
+      .then(function (result) {
+        if (opts.modularize) {
+          self.push('module.exports = ' + exports + ';' + result)
+        } else {
+          self.push('module.exports = ' + result)
+        }
         done()
       }, function (err) {
         self.emit('error', err)
@@ -48,16 +78,27 @@ function base64 (css) {
   return 'data:text/css;base64,' + css
 }
 
-function moduleify (css, inject) {
+function moduleify (css, modulename) {
+  return new Core()
+    .load(css, modulename)
+    .then(function (result) {
+      exports = JSON.stringify(result.exportTokens)
+      return result.injectableSource
+    })
+}
+
+function insert (css, inject) {
   var exp
   if (inject === 'base64') {
     exp = 'require("browserify-postcss").byUrl("' + base64(css) + '")'
+  } else if (inject === 'insert-css') {
+    exp = "require('insert-css')('" + css.replace(/\\/g, '\\\\').replace(/'/gm, "\\'").replace(/[\r\n]+/gm, ' ') + "')"
   } else if (inject) {
     exp = "require('browserify-postcss')('" + css.replace(/\\/g, '\\\\').replace(/'/gm, "\\'").replace(/[\r\n]+/gm, ' ') + "')"
   } else {
     exp = JSON.stringify(css)
   }
-  return 'module.exports = ' + exp
+  return exp
 }
 
 function createProcessor (opts) {
@@ -71,7 +112,7 @@ function createProcessor (opts) {
       if (typeof p === 'string') {
         p = require(
           resolve.sync(String(p), {
-            basedir: opts.basedir || process.cwd()
+            basedir: opts.basedir || process.cwd(),
           })
         )
       }
